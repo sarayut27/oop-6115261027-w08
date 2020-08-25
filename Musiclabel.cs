81200ab2cdee828d46c0ed64b37c09c74d241fd4
sarayut27 <68211053+sarayut27@users.noreply.github.com>
2020-08-25 23:52:24 +0700

    Add files via upload

---
+++ Musiclabel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace oop_6115261027_w08
+{
+    class Musiclabel
+    {
+        private string name;
+        private string addass;
+        private string songWriter;
+        private Singer singerMusic;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Addass
+        {
+            get { return addass; }
+            set { addass = value; }
+        }
+        public string SongWriter
+        {
+            get { return songWriter; }
+            set { songWriter = value; }
+        }
+        public Singer SingerMusic
+        {
+            get { return singerMusic; }
+            set { singerMusic = value; }
+        }
+        public Musiclabel() { }
+        public Musiclabel(string n, string ad, string sw, Singer sm)
+        {
+            this.name = n;
+            this.addass = ad;
+            this.SongWriter = sw;
+            this.singerMusic = sm;
+
+        }
+        public override string ToString()
+        {
+            return this.Name + " " +
+                 this.Addass + " " +
+                 this.SongWriter + " " +
+                 this.SingerMusic.Name;
+
+        }
+    }
+}
---
+++ Singer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace oop_6115261027_w08
+{
+    class Singer
+    {
+        private string name;
+        private string nickName;
+        private string age;
+        private string birthDay;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string NickName
+        {
+            get { return nickName; }
+            set { nickName = value; }
+        }
+        public string Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+        public string BirthDay
+        {
+            get { return birthDay; }
+            set { birthDay = value; }
+        }
+        public Singer() { }
+        public Singer(string n, string nn, string a, string b)
+        {
+            this.name = n;
+            this.nickName = nn;
+            this.age = a;
+            this.birthDay = b;
+
+        }
+        public override string ToString()
+        {
+            return this.Name + " " +
+                 this.NickName + " " +
+                 this.Age + " " +
+                 this.BirthDay;
+
+        }
+    }
+}
