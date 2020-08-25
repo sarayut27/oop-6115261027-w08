d7da4a07db2233320497bd29cd64623169a86a94
sarayut27 <68211053+sarayut27@users.noreply.github.com>
2020-08-26 01:27:36 +0700

    Add files via upload

---
+++ Musiclabel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace oop_6115261027_w08
+{
+    class Musiclabel
+    {
+        private string name;
+        private string addass;
+        private string executive;
+        private Singer singerMusic;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Addass
+        {
+            get { return addass; }
+            set { addass = value; }
+        }
+        public string Executive
+        {
+            get { return executive; }
+            set { executive = value; }
+        }
+        public Singer SingerMusic
+        {
+            get { return singerMusic; }
+            set { singerMusic = value; }
+        }
+        public Musiclabel() { }
+        public Musiclabel(string n, string ad, string ec, Singer sm)
+        {
+            this.name = n;
+            this.addass = ad;
+            this.executive = ec;
+            this.singerMusic = sm;
+
+        }
+        public override string ToString()
+        {
+            return this.Name + " " +
+                 this.Addass + " " +
+                 this.executive + " " +
+                 this.SingerMusic.Name + " " +
+                 this.SingerMusic.NickName;
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace oop_6115261027_w08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singer s1 = new Singer("Yerim" , "Yeri" , "21" , "1999 March 5");
+            Singer s2 = new Singer("Suyong" , "Joy" , "23" , "1996 September 3");
+            Singer s3 = new Singer("Sungwan" , "Wendy" , "21" , "1994 February 21 ");
+            Musiclabel m1 = new Musiclabel("SM" , "Korea" , "LeeSooMan" , s2);
+            Console.WriteLine(m1);
+        }
+    }
+}
